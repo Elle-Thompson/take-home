38475eb40075b6aec69d70a2f8acdc3d85948560
Elle Thompson <elletee@Elles-iMac.local>
2022-02-05 15:47:58 -0800

    correctly removed NavBar from splash pash and added cards for feed page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import {Container} from "react-bootstrap";
 import {
   Outlet
 } from "react-router-dom";
-import MainNav from "./components/MainNav";
+
 
 function App() {
   return (
     <>
-      <MainNav/>
+      
       <Container>
         <Outlet/>
       </Container>
